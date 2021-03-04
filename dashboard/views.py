6be4d934a2bd51b14b28eef3d8382efcdf5d34be
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-03-04 17:39:53 +0700

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -1127,6 +1127,8 @@ def hitungskor(user_id):
 	tp = TugasProyek.objects.filter(pemilik_tugas=pemilik)
 	tr = TugasRutin.objects.filter(pemilik_tugas=pemilik)
 	
+    # tugas_rutin = [a for a in tr]
+
 	total_skor_rutin = 0
 	for rutin in tr:
 		isi_tr = IsiTugasRutin.objects.filter(tugas_rutin = rutin)
@@ -1139,4 +1141,9 @@ def hitungskor(user_id):
 		if tugas_proyek.status == 'Tuntas':
 			total_skor_proyek += tugas_proyek.penilaian
 	
-	return (total_skor_proyek + total_skor_rutin)/(len(tp) + len(tr))
+    if (total_skor_rutin == 0) or (total_skor_proyek == 0):
+        hasil = 0
+    else:
+        hasil = (total_skor_proyek + total_skor_rutin)/(len(tp) + len(tr))
+
+	return hasil
