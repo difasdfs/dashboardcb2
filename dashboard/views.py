3501e5fdb525e3df8fce4189bfe1943b2e662e5a
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-03-09 21:52:32 +0700

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -109,7 +109,7 @@ def klasemen(request):
         if a.id not in id_terlarang:
             user.append(a)
 
-    id_orang_office = [6,3,23,25,8,22,2,24,4,31,21]
+    id_orang_office = [6,3,23,25,8,22,2,24,4,31]
     orang_office = []
     non_office = []
 
