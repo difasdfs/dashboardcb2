74c705935af792a58d8cd047bf3c5323a9fa08f1
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-03-24 15:09:45 +0700

    Add files via upload

--- dashboardcb/settings.py
+++ dashboardcb/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler", "django_excel.TemporaryExcelFileUploadHandler")
