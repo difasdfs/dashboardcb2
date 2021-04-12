cd0aceac5d24b401a418eb513043778093dfdbf4
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-04-12 13:06:36 +0700

    Add files via upload

--- dashboard/logic.py
+++ dashboard/logic.py
@@ -8,6 +8,13 @@ def anggotabagian(nama_manager, bagian):
 def apamanager(user):
     return user.groups.filter(name='Manager').exists()
 
+def berselisih(variabel1, variabel2):
+    hasil = variabel1 - variabel2
+    if hasil > 0:
+        return '+' + str(hasil)
+    else:
+        return str(hasil)
+
 def fungsi_helper_untuk_fungsi_di_bawah(cabangnya, complaint_periode, complaint_periode_sebelumnya):
     
     if cabangnya != "All Crisbar":
@@ -39,13 +46,14 @@ def fungsi_helper_untuk_fungsi_di_bawah(cabangnya, complaint_periode, complaint_
     tidak_lengkap_cabang = len(tidak_lengkap_cabang)
     tidak_lengkap_cabang_sebelumnya = complaint_cabang_sebelumnya.filter(jenis="Tidak Lengkap")
     tidak_lengkap_cabang_sebelumnya = len(tidak_lengkap_cabang_sebelumnya)
-
+        
     cabang = [
-        (kualitas_produk_cabang, str(kualitas_produk_cabang - kualitas_produk_cabang_sebelumnya)),
-        (kebersihan_cabang, str(kebersihan_cabang-kebersihan_cabang_sebelumnya)),
-        (pelayanan_cabang, str(pelayanan_cabang-pelayanan_cabang_sebelumnya)),
-        (tidak_lengkap_cabang, str(tidak_lengkap_cabang-tidak_lengkap_cabang_sebelumnya)),
-        (total_complaint_cabang, str(total_complaint_cabang-total_complaint_cabang_sebelumnya))
+        (cabangnya, ''),
+        (kualitas_produk_cabang, berselisih(kualitas_produk_cabang, kualitas_produk_cabang_sebelumnya)),
+        (kebersihan_cabang, berselisih(kebersihan_cabang, kebersihan_cabang_sebelumnya)),
+        (pelayanan_cabang, berselisih(pelayanan_cabang, pelayanan_cabang_sebelumnya)),
+        (tidak_lengkap_cabang, berselisih(tidak_lengkap_cabang, tidak_lengkap_cabang_sebelumnya)),
+        (total_complaint_cabang, berselisih(total_complaint_cabang, total_complaint_cabang_sebelumnya)),
     ]
 
     return cabang
@@ -54,19 +62,14 @@ def query_complaint_dashboard(PERIODE):
     periode = PeriodeKerja.objects.get(pk=PERIODE)
     periode_sebelumnya = PeriodeKerja.objects.get(pk=(PERIODE-1))
     
+    CABANG = ['Antapani', 'Cisitu', 'Jatinangor', 'Metro', 'Sukabirus', 'Sukapura', 'Sukajadi', 'Unjani', 'All Crisbar']
+
     c = Complaint.objects.all()
     c = c.exclude(jenis="Lainnya")
     complaint_periode = c.filter(tanggal__range=[periode.awal_periode, periode.akhir_periode])
     complaint_periode_sebelumnya = c.filter(tanggal__range=[periode_sebelumnya.awal_periode, periode_sebelumnya.akhir_periode])
-
-    antapani = fungsi_helper_untuk_fungsi_di_bawah("Antapani", complaint_periode, complaint_periode_sebelumnya)
-    cisitu = fungsi_helper_untuk_fungsi_di_bawah("Cisitu", complaint_periode, complaint_periode_sebelumnya)
-    jatinangor = fungsi_helper_untuk_fungsi_di_bawah("Jatinangor", complaint_periode, complaint_periode_sebelumnya)
-    metro = fungsi_helper_untuk_fungsi_di_bawah("Metro", complaint_periode, complaint_periode_sebelumnya)
-    sukabirus = fungsi_helper_untuk_fungsi_di_bawah("Sukabirus", complaint_periode, complaint_periode_sebelumnya)
-    sukapura = fungsi_helper_untuk_fungsi_di_bawah("Sukapura", complaint_periode, complaint_periode_sebelumnya)
-    sukajadi = fungsi_helper_untuk_fungsi_di_bawah("Sukajadi", complaint_periode, complaint_periode_sebelumnya)
-    unjani = fungsi_helper_untuk_fungsi_di_bawah("Unjani", complaint_periode, complaint_periode_sebelumnya)
-    all_crisbar = fungsi_helper_untuk_fungsi_di_bawah("All Crisbar", complaint_periode, complaint_periode_sebelumnya)
+    
+    complaint_cabang = [fungsi_helper_untuk_fungsi_di_bawah(c, complaint_periode, complaint_periode_sebelumnya) for c in CABANG]
+    antapani, cisitu, jatinangor, metro, sukabirus, sukapura, sukajadi, unjani, all_crisbar = complaint_cabang
 
     return antapani, cisitu, jatinangor, metro, sukabirus, sukapura, sukajadi, unjani, all_crisbar
