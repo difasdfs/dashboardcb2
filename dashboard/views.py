8f91495776e0a467db70e8d6f0f758c749bf79e1
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-04-29 02:35:56 +0700

    Add files via upload

--- dashboard/views.py
+++ dashboard/views.py
@@ -15,7 +15,7 @@ from .periode_sp import evaluasi, dapet_sp_periode_ini
 from .rekap import query_rekap
 from .rinci_tugas_rutin import rinci_tr, rinci_tr_eksekutif
 from .supply_chain import update_pemakaian_ayam, periksa_hari_dalam_pemakaian_ayam, query_rata_rata_deman_ayam
-from .operation import penentu_awal_akhir_jam
+from .operation import penentu_awal_akhir_jam, query_home_operation
 from .models import *
 
 from django.utils import timezone
@@ -2741,9 +2741,8 @@ def index_supply_chain(request):
 def index_operation(request):
     
     context = {'nama' : request.user.first_name}
-    awal, akhir = penentu_awal_akhir_jam(date(2021, 4, 27), 11)
-    print(awal)
-    print(akhir)
+    hasilnya = query_home_operation()
+    context['home_operation'] = hasilnya
 
     if not request.user.groups.filter(name='Eksekutif').exists() or request.user.last_name == 'Human Resource':
         context['data_kar'] = True
