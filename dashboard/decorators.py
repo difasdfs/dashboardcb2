b12cf976b4d1d5754c64114afa2a53e47de39816
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-05-06 18:51:05 +0700

    Add files via upload

--- dashboard/decorators.py
+++ dashboard/decorators.py
@@ -4,7 +4,7 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('manager')
+            return redirect('index_rm')
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
