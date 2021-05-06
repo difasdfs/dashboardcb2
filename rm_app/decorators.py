86b5014ed6feff7f5ef98af2e4c483b545a71736
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-05-06 18:53:19 +0700

    Add files via upload

--- rm_app/decorators.py
+++ rm_app/decorators.py
@@ -1,10 +1,12 @@
 from django.shortcuts import redirect
-
+from rm_app.logika import isrm
+from rm_app.logika.isrm import is_rm
 
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
+        is_rm(request.user)
         if request.user.is_authenticated:
-            return redirect('manager')
+            return redirect('index_rm')
         else:
             return view_func(request, *args, **kwargs)
     return wrapper_func
