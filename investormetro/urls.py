7a6cafec69de44e2759329896a074e8afcbca750
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-06-18 15:15:58 +0700

    Add files via upload

--- investormetro/urls.py
+++ investormetro/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('halaman_login/', views.loginpage, name='halaman_login'),
+    path('halaman_login/', views.halaman_login, name='halaman_login'),
     path('logout/', views.logoutuser, name='logout'),
     path('', views.index, name='indexcrisbarmetro'),
 ]
