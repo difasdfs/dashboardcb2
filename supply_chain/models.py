2a3eb17c655d27911e9b5fcb0b29b0ed3cb4eeeb
Difa Surya <40395966+difasdfs@users.noreply.github.com>
2021-07-02 16:21:56 +0700

    Add files via upload

--- supply_chain/models.py
+++ supply_chain/models.py
@@ -8,3 +8,10 @@ class KategoriLoyverse(models.Model):
 
     def __str__(self):
         return self.nama_kategori
+
+class CabangLoyverse(models.Model):
+    id_loyverse = models.CharField(max_length=80)
+    nama_cabang = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.nama_cabang + " - " + self.id_loyverse
